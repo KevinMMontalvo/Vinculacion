a823c8e25f82a4c6cea2bea694a5748b17681aff
Mateo1309 <meteoro.1309@gmail.com>
2018-11-29 04:45:46 -0500

    GUI login completa

--- codigo/AulaMultisensorialWeb/imports/components/SideMenu.js
+++ codigo/AulaMultisensorialWeb/imports/components/SideMenu.js
@@ -4,15 +4,24 @@ export default class SideMenu extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-
+          isLogged: true,
         }
     }
     render() {
         return(
             <div>
               <div className="side-menu">
+                {this.state.isLogged ?
+                  <div className="menu-options">
+                    <div id="option1" className="menu-option">Estudiantes</div>
+                    <div id="option2" className="menu-option">Docentes</div>
+                    <div id="option3" className="menu-option">Niveles</div>
+                    <div id="option4" className="menu-option">Registro de actividades</div>
+                  </div>
+                  :
+                  <div className="menu-icon"></div>
+                }
                 <div className="menu-title">FUVIME</div>
-                <div className="menu-icon"></div>
                 <div className="user-info-container"></div>
               </div>
             </div>
